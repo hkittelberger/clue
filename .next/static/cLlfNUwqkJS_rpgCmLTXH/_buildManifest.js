ea76248305e0ae7724fa3e3803d4bca0c8a8d270
hkittelberger <holdenkittelberger@gmail.com>
2024-08-01 17:03:43 -0500

    hello world

---
+++ .next/static/cLlfNUwqkJS_rpgCmLTXH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-11cbd057a7d008eb.js"],"/_error":["static/chunks/pages/_error-704236eec9add211.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
